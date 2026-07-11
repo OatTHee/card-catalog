49dc65364bc4ce26269e0a0225e0c0e820781763
OatTHee <163233050+OatTHee@users.noreply.github.com>
2026-07-11 16:46:04 +0700

    kuylek

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co', // เก็บไว้ชั่วคราวช่วง migrate รูปเก่า
-      },
+
       {
         protocol: 'https',
         hostname: 'pub-b5f616fbd8a1437d8fd9a58a37363569.r2.dev',
